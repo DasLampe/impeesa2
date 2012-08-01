0da0302f12b2a3cf6b8cfdd45fb84045d089709d
DasLampe <daslampe@lano-crew.org>
2012-08-01 02:34:18 +0200

    ADD: add buttons to infobar
    You can add buttons to the infobar, e.g. button to upload picture.
    This buttons placed in a grid, so they in one line and don't overflow
    the infobar.

--- core/controller/page.controller.php
+++ core/controller/page.controller.php
@@ -25,12 +25,7 @@ class pageController
 			include_once(PATH_CONTROLLER."sidebar.php");
 			$sidebar				= new SidebarController($this->param);
 			$tpl->vars("sidebar",	$sidebar->factoryController());
-			
-			//Infobar
-			include_once(PATH_CONTROLLER."infobar.php");
-			$infobar				= new InfobarController($this->param);
-			$tpl->vars("infobar",	$infobar->factoryController());
-	
+
 			//Content
 			if(file_exists(PATH_CONTENT.$this->param[0].".php") && !file_exists(PATH_CONTROLLER.$this->param[0].".php"))
 			{
@@ -54,6 +49,15 @@ class pageController
 		{
 			$this->tpl->vars("page_content", $e->getCustomMessage());
 		}
+		
+		/**
+		 * Infobar
+		 * @info: placed here, because you can set some buttons in the skript 
+		 */
+		include_once(PATH_CONTROLLER."infobar.php");
+		$infobar				= new InfobarController($this->param);
+		$tpl->vars("infobar",	$infobar->factoryController());
+		
 		
 		/**
 		 * Handle JSON
