a8e309be2e23a1ab792c78c90cf58c09a7635b41
DasLampe <daslampe@lano-crew.org>
2013-01-25 02:05:56 +0100

    MODIFIED: Picture test case.
    New:
    - upload picture
    - remove picture
    - remove album
    - get picture from album

--- tests/cases/picture.test.php
+++ tests/cases/picture.test.php
@@ -3,7 +3,7 @@
 // | Copyright (c) 2013 DasLampe <daslampe@lano-crew.org> |
 // | Encoding:  UTF-8 |
 // +----------------------------------------------------------------------+
-class TestPicture extends UnitTestCases {
+class TestPicture extends UnitTestCase {
 	private $model;
 	private $album_name;
 	
@@ -11,6 +11,7 @@ class TestPicture extends UnitTestCases {
 		include_once(PATH_MODEL."picture.php");
 		$this->model		= new PictureModel();
 		$this->album_name	= "0000_UnitTestAlbum";
+		$this->filename		= "pictureTest.jpg";
 	}
 	
 	public function testCreateAlbum() {
@@ -20,13 +21,29 @@ class TestPicture extends UnitTestCases {
 	}
 	
 	public function testUploadPicture() {
-		/**
-		* FIND A WAY
-		**/
+		//Fake $_FILE
+		$array = array(
+					'name'		=> $this->filename,
+					'type'		=> "image/jpeg",
+					'tmp_name'	=> dirname(__FILE__)."/share/".$this->filename,
+					'error'		=> 0,
+					'size'		=> filesize(dirname(__FILE__)."/share/".$this->filename),
+				);
+		$this->assertTrue($this->model->UploadPicture($array, PATH_UPLOAD."picture/".$this->album_name));
+		//Realy uploaded?
+		$this->assertTrue(file_exists(PATH_UPLOAD."picture/".$this->album_name."/".$this->filename));
 	}
 	
 	public function testGetAlbumPicture() {
-		//Needed Upload Picture
+		$album	= $this->model->GetAlbumPicture($this->album_name);
+
+		$this->assertFalse(empty($album));
+	}
+	
+	public function testRemovePicture() {
+		$this->assertTrue($this->model->DeletePicture(PATH_UPLOAD."picture/".$this->album_name, $this->filename));
+		
+		$this->assertFalse(file_exists(PATH_UPLOAD."picture/".$this->album_name."/".$this->filename));
 	}
 	
 	public function testRemoveAlbum() {
--- tests/run.php
+++ tests/run.php
@@ -10,12 +10,14 @@ include_once(dirname(__FILE__)."/simpletest/autorun.php");
 //Tests
 include_once(dirname(__FILE__)."/cases/user.test.php");
 include_once(dirname(__FILE__)."/cases/pageAction.test.php");
+include_once(dirname(__FILE__)."/cases/picture.test.php");
 
 class AllTests extends TestSuite {
 	function _construct() {
 		parent::_construct();
 		$this->addTest(new TestUser());
 		$this->addTest(new TestPageAction());
+		$this->addTest(new TestPicture());
 	}
 }
 
