5d09f19d987cc99e4083552f74a4269b6ed44091
DasLampe <daslampe@lano-crew.org>
2013-01-28 02:06:07 +0100

    MODIFIED: Track db migration sql files.

---
+++ db_migration/2.0.4.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS `impeesa2_groups` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `description` text NOT NULL,
+  `day` text NOT NULL,
+  `youngest` int(2) NOT NULL,
+  `oldest` int(3) NOT NULL,
+  `begin` time NOT NULL,
+  `end` time NOT NULL,
+  `logo` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+UPDATE  `impeesa2_config` SET  `config_value` =  '2.0.4' WHERE  `config_key` =  'version';
